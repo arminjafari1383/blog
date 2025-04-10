b327d1a2d8957874ac04da3fd723ab47e5efc0b0
arminjafari1383 <your_github_email>
2025-04-10 02:02:43 -0600

    feature:customize dashboard admin django

--- sazweb/blog/admin.py
+++ sazweb/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','author','publish','status']
+    ordering = ['title','publish']
+    list_filter = ['status','author','publish']
+    search_fields = ['title','description']
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    prepopulated_fields = {"slug" : ['title']}
+    list_editable = ['status']
+    list_display_links = ['author']
