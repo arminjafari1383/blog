49ae23954172cea4642b9f1506100e440ce53073
arminjafari1383 <your_github_email>
2025-04-12 08:35:46 -0600

    feature:create paginator

--- sazweb/blog/models.py
+++ sazweb/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 #Mangers
 class PublishedManger(models.Manager):
@@ -36,4 +37,7 @@ class Post(models.Model):
         ]
 
     def __str__(self):
-        return self.description
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',args=[self.id])
