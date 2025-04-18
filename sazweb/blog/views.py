053b62fc7f20d54006b6d1cf14b4326f904798eb
arminjafari1383 <your_github_email>
2025-04-17 23:28:17 -0600

    feature:Q objects(advance search)

--- sazweb/blog/views.py
+++ sazweb/blog/views.py
@@ -5,6 +5,7 @@ from .forms import *
 # from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.views.generic import ListView,DetailView
 from django.views.decorators.http import require_POST
+from django.db.models import Q
 
 
 # Create your views here.
@@ -95,9 +96,7 @@ def post_search(request):
         form = SearchForm(data=request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results1 = Post.published.filter(title__icontains = query)
-            results2 = Post.published.filter(description__icontains = query)
-            results = results1 ^ results2
+            results = Post.published.filter(Q(title__icontains = query) | Q(description__icontains = query))
     else:
         form = SearchForm()
 
